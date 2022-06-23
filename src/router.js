03feae4b9a1a032e2c11653b6c6baac036195b22
Affonsok <affonsok@gmail.com>
2022-06-23 14:19:32 -0300

    first commit

---
+++ src/router.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+
+const routes = new Router();
+
+routes.get("/", (req, res) => {
+    return res.json({message: "okay"})
+
+});
+
+module.exports = routes;
+
+
+
+
